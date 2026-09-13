39bbb76deff84132072da2daf9b5eb8091c12d5a
vidya091209 <vidya.091209@gmail.com>
2026-09-13 12:47:30 +0530

    Update index.ts
    updated logs

--- supabase/functions/whatsapp-dispatcher/index.ts
+++ supabase/functions/whatsapp-dispatcher/index.ts
@@ -411,8 +411,10 @@ async function postPayloadToWhatsAppEndpoint({
 Deno.serve(async (req) => {
   const authHeader = req.headers.get("Authorization");
   const token = authHeader?.replace("Bearer ", "");
-
+ log.info(`Dispatching message SERVICE_ROLE_KEY`, SERVICE_ROLE_KEY);
+    log.info(`token not matching`, token);
   if (token !== SERVICE_ROLE_KEY) {
+     log.info(`token not matching`, token);
     return new Response("Unauthorized", { status: 401 });
   }
 
